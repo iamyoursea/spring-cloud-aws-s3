12a8f2bb2091160ae7cb0fc18608a58835da1cb9
seungh0 <will.seungho@gmail.com>
2020-07-04 04:48:00 +0900

    개발환경 세팅

---
+++ src/test/java/will/seungho/springawss3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package will.seungho.springawss3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
